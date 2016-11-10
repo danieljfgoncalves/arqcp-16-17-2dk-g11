bfa03ed1da21c0f32636327b4751fceefb2d4ae9
Tiagofsc <1151031@isep.ipp.pt>
2016-11-10 22:57:49 +0000

    Completes exercise 3 from module 4 (md04-ex03)

---
+++ modulo4/ex03/greater.h
@@ -0,0 +1,17 @@
+/*
+ * Tiago Correia - 1151031@isep.ipp.pt
+ * ARQCP - Turma 2DK
+ * 
+ * GREATER.H
+ * 
+ */
+
+#ifndef GREATER_H
+#define GREATER_H
+
+/*
+ * Returns the greatest of three numbers passed as parameters.
+ */
+int greater(int a, int b, int c);
+
+#endif
