be5d53b74c14cbe8adff116ab5504384df7f3860
Daniel Goncalves <me@danieljgoncalves.com>
2016-11-10 23:20:54 +0000

    Completes exercise 10b from module 4 (ex10b-md04)

---
+++ modulo4/ex10b/count_bits_zero.h
@@ -0,0 +1,20 @@
+/*
+ * Daniel Goncalves > 1151452@isep.ipp.pt
+ * ARQCP - Turma 2DK
+ * 
+ * count_bits_zero.h
+ * 
+ */
+
+#ifndef COUNT_BITS_ZERO_H
+#define COUNT_BITS_ZERO_H
+
+/**
+ * Count the number of inactive bits (value 0) in a integer value.
+ * 
+ * x - integer to verify.
+ * 
+ * Returns the number of inactive bits.
+ */
+int count_bits_zero(int x);
+#endif
---
+++ modulo4/ex10b/count_bits_zero.s
@@ -0,0 +1,41 @@
+/*
+ * Daniel Goncalves > 1151452@isep.ipp.pt
+ * ARQCP - Turma 2DK
+ * 
+ * count_bits_zero.s
+ * 
+ */
+
+.section .data
+
+.section .text
+.global count_bits_zero    	# int count_bits_zero(int x)
+
+count_bits_zero:
+
+# prologue
+	pushl %ebp      	# save previous stack frame pointer
+	movl %esp, %ebp  	# the stack frame pointer for sum function
+	
+# body of the function
+	
+	movl $0, %eax		# clears eax
+	
+	movl 8(%ebp), %ebx	# moves 1st param to ebx
+
+while:	
+	cmpl $0, %ebx		# compares 0 to ebx (ebx - 0)
+	je end				# if 0 jump to end
+	
+	shr %ebx			# shifts ebx by 1 to the right (ebx >> 1) (no need to preserve sign)
+	jc skip				# if CF=1 go to skip
+	incl %eax			# increments by 1
+	
+skip:
+	jmp while			# jumps to while
+	
+end:
+# epilogue
+	movl %ebp, %esp  	# restore the previous stack pointer ("clear" the stack)
+	popl %ebp     		# restore the previous stack frame pointer
+	ret
