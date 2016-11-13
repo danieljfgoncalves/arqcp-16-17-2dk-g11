5770dfebdcca596ad3529df7ed14fbf9697e630d
Tiagofsc <1151031@isep.ipp.pt>
2016-11-13 22:50:58 +0000

    Completes exercise 15b from module 4 (md04-ex15b)

---
+++ modulo4/ex15b/greater_date.h
@@ -0,0 +1,21 @@
+/*
+ * Tiago Correia - 1151031@isep.ipp.pt
+ * ARQCP - Turma 2DK
+ * 
+ * NAME.H
+ * 
+ */
+
+#ifndef GREATER_DATE_H
+#define GREATER_DATE_H
+
+/*
+ * Returns the greater of two dates.
+ * 
+ * int date1 - first date
+ * int date2 - second date
+ * 
+ */
+int greater_date(int date1, int date2);
+
+#endif
