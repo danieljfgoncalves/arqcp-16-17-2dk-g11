259f5305eb09f68331aaf2fed0b209a8b49cd0d3
Tiagofsc <1151031@isep.ipp.pt>
2016-11-18 20:59:57 +0000

    Completes exercise 17a from module 4 (md04-ex17a)

---
+++ modulo4/ex17a/add_byte.h
@@ -0,0 +1,17 @@
+/*
+ * Tiago Correia - 1151031@isep.ipp.pt
+ * ARQCP - Turma 2DK
+ * 
+ * add_byte.h
+ * 
+ */
+
+/*
+ * Adds a char to the first byte of each element of an array of integers and stores the result in a second array of integers.
+ * 
+ * int x - the given char
+ * int *vec1 - the address of the first array
+ * int *vec2 - the address of the second array
+ * 
+ */
+void add_byte(char x, int *vec1, int *vec2);
